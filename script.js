a215f096b78390316461c10553c250cf94cc7065
Huseyinbiyik <tahahuseyinb@gmail.com>
2022-04-04 16:32:31 +0300

    Add js to mobile navigation

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuNav = document.querySelector('.nav-bar');
+const menuBtn = document.querySelector('.bars-menu');
+
+menuBtn.addEventListener('click', () => {
+  menuNav.classList.toggle('active');
+
+})
+
+const navLink = document.getElementsByClassName('link');
+
+// document.body.classList.toggle('overflow-disabled');
+
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener('click', () => {
+    menuNav.classList.toggle('active')
+  });
+}
