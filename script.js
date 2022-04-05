9c3a73095382150d24ed4772e247c9e41cbcc37b
ndayishimiyeeric <82580160+ndayishimiyeeric@users.noreply.github.com>
2022-04-05 11:44:46 +0200

    Add object store functionality

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ while (i < navLink.length) {
 }
 // Full page navigation menu ends
 
-let works = {
+
+
+let work1 = {
   item_title: "Tonic",
   item_image: "./img/work1.svg",
   item_history: {
@@ -40,8 +42,8 @@ let works = {
   project_see_button: "See Project",
 }
 
-let works2 = {
-  item_title: "Tonic",
+let work2 = {
+  item_title: "Multi-Post Stories",
   item_image: "./img/work2.svg",
   item_history: {
     t_bold: "Canopy",
@@ -59,36 +61,82 @@ let works2 = {
   project_see_button: "See Project",
 }
 
-let item_title = document.querySelector('.item-title');
-let item_image = document.querySelector('.item-img-1');
-let t_bold = document.querySelector('.t-bold');
-let counter1 = document.querySelector('.counter1');
-let backend = document.querySelector('.backend');
-let counter2 = document.querySelector('.counter2');
-let year = document.querySelector('.year');
-let item_description_text = document.querySelector('.item-description-text');
-let first_category = document.querySelector('.first-category');
-let second_category = document.querySelector('.second-category');
-let third_category = document.querySelector('.third-category');
-let project_see_button = document.querySelector('.project-one-button');
+let work3 = {
+  item_title: "Tonic",
+  item_image: "./img/work3.svg",
+  item_history: {
+    t_bold: "Canopy",
+    counter1: "./img/Counter.svg",
+    backend: "Back End Dev",
+    counter2: "./img/Counter.svg",
+    year: 2022
+  },
+  item_description_text: 'A daily selection of privately personalized reads;no accounts or sign - ups required.',
+  item_categories: {
+    first_category: "html",
+    second_category: "css",
+    third_category: "javascript"
+  },
+  project_see_button: "See Project",
+}
+
+let work4 = {
+  item_title: "Multi-Post Stories",
+  item_image: "./img/work4.svg",
+  item_history: {
+    t_bold: "Canopy",
+    counter1: "./img/Counter.svg",
+    backend: "Back End Dev",
+    counter2: "./img/Counter.svg",
+    year: 2022
+  },
+  item_description_text: 'A daily selection of privately personalized reads;no accounts or sign - ups required.',
+  item_categories: {
+    first_category: "html",
+    second_category: "css",
+    third_category: "javascript"
+  },
+  project_see_button: "See Project",
+}
+
+let item_title = document.getElementsByClassName('item-title');
+let item_image = document.getElementsByClassName('item-img-1');
+let t_bold = document.getElementsByClassName('t-bold');
+let counter1 = document.getElementsByClassName('counter1');
+let backend = document.getElementsByClassName('backend');
+let counter2 = document.getElementsByClassName('counter2');
+let year = document.getElementsByClassName('year');
+let item_description_text = document.getElementsByClassName('item-description-text');
+let first_category = document.getElementsByClassName('first-category');
+let second_category = document.getElementsByClassName('second-category');
+let third_category = document.getElementsByClassName('third-category');
+let project_see_button = document.getElementsByClassName('project-one-button');
 
 
-item_title.innerHTML = works.item_title;
-item_image.src = works.item_image;
-t_bold.innerHTML = works.item_history.t_bold;
-counter1.src = works.item_history.counter1;
-backend.innerHTML = works.item_history.backend;
-counter2.src = works.item_history.counter2;
-year.innerHTML = works.item_history.year;
-item_description_text.innerHTML = works.item_description_text;
-first_category.innerHTML = works.item_categories.first_category;
-second_category.innerHTML = works.item_categories.second_category;
-third_category.innerHTML = works.item_categories.third_category;
-project_see_button.innerHTML = works.project_see_button;
+function worksAssignment(workItem) {
+  // item_title.innerHTML = workItem.item_title;
+  for (let i = 0; i < item_title.length; i++){
+    item_title[i].innerHTML = workItem.item_title[i];
+  }
+    item_image.src = workItem.item_image;
+    t_bold.innerHTML = workItem.item_history.t_bold;
+    counter1.src = workItem.item_history.counter1;
+    backend.innerHTML = workItem.item_history.backend;
+    counter2.src = workItem.item_history.counter2;
+    year.innerHTML = workItem.item_history.year;
+    item_description_text.innerHTML = workItem.item_description_text;
+    first_category.innerHTML = workItem.item_categories.first_category;
+    second_category.innerHTML = workItem.item_categories.second_category;
+    third_category.innerHTML = workItem.item_categories.third_category;
+    project_see_button.innerHTML = workItem.project_see_button;
+}
 
-let myWorks = [works, works2];
-let secondWork = document.querySelector(".item-2");
-let firstWork = document.querySelector(".item-1");
+worksAssignment(work1)
+worksAssignment(work2)
+
+// let myWorks = [works, works2];
+// let secondWork = document.querySelector(".item-2");
+// let firstWork = document.querySelector(".item-1");
 
 
 
